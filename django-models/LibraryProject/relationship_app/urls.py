c4882c6fc064bca978f3efda4fcc7974c52e767e
Sara <sarasarra36@icloud.com>
2025-09-06 14:59:06 +0100

    Task4

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from . import views
-from .views import list_books
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-
+    # Authentification
     path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
     path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
     path("register/", views.register_view, name="register"),
 
+    # Livres et bibliothèques
     path("books/", views.list_books, name="list_books"),
     path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
 
@@ -16,11 +17,8 @@ urlpatterns = [
     path("librarian-view/", views.librarian_view, name="librarian_view"),
     path("member-view/", views.member_view, name="member_view"),
 
-    path("books/add/", views.add_book, name="add_book"),
-    path("books/<int:book_id>/edit/", views.edit_book, name="edit_book"),
-    path("books/<int:book_id>/delete/", views.delete_book, name="delete_book"),
-
-    path("admin-view/", views.admin_view, name="admin_view"),
-    path("librarian-view/", views.librarian_view, name="librarian_view"),
-    path("member-view/", views.member_view, name="member_view"),
+    # Permissions personnalisées
+    path("add_book/", views.add_book, name="add_book"),
+    path("edit_book/<int:pk>/", views.edit_book, name="edit_book"),
+    path("delete_book/<int:pk>/", views.delete_book, name="delete_book"),
 ]
