4b3f0233be264ba53282e581a0016673cfd960b3
Sara <sarasarra36@icloud.com>
2025-09-06 14:26:41 +0100

    Task3:Implement Role-Based Access Control in Django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import DetailView
 from .models import Book
 from .models import Library
@@ -6,6 +6,7 @@ from django.views.generic.detail import DetailView
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import permission_required
 
 def list_books(request):
     books = Book.objects.all()
@@ -66,3 +67,32 @@ def librarian_view(request):
 @user_passes_test(is_member)
 def member_view(request):
     return render(request, "relationship_app/member_view.html")
+# Ajouter un livre
+@permission_required("relationship_app.can_add_book", raise_exception=True)
+def add_book(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        author_id = request.POST.get("author")
+        if title and author_id:
+            Book.objects.create(title=title, author_id=author_id)
+            return redirect("list_books")
+    return render(request, "relationship_app/add_book.html")
+
+# Modifier un livre
+@permission_required("relationship_app.can_change_book", raise_exception=True)
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == "POST":
+        book.title = request.POST.get("title", book.title)
+        book.save()
+        return redirect("list_books")
+    return render(request, "relationship_app/edit_book.html", {"book": book})
+
+# Supprimer un livre
+@permission_required("relationship_app.can_delete_book", raise_exception=True)
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == "POST":
+        book.delete()
+        return redirect("list_books")
+    return render(request, "relationship_app/delete_book.html", {"book": book})
